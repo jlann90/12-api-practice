73fe4c59e046ac98877a41f8465d82e6ebc905ad
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-08-25 14:54:20 -0700

    feat: add API call to fetch places, use setAvailablePlaces to update our availablePlaces state, wrap API call in useEffect to prevent an infinite loop - it only runs once the first time the AvailablePlaces component is rendered and poplates the availablePlaces state

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,25 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    // fetch places from the server
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        // set the places in the state
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
